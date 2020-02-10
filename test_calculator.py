8f89c5f459e6e99aa147688df6644692ec678447
hkitapci-swe <60547436+hkitapci-swe@users.noreply.github.com>
2020-02-10 23:44:11 +0300

    fixed spacing

--- test_calculator.py
+++ test_calculator.py
@@ -17,4 +17,4 @@ class TestCalculator:
         assert 100 == calculator.multiply(10, 10)
 
     def test_division(self):
-        assert  5 == calculator.divide(10, 2)
+        assert 5 == calculator.divide(10, 2)
