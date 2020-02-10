08ba48c2908a2aa31a0fc11371dee53285f6182b
hkitapci-swe <60547436+hkitapci-swe@users.noreply.github.com>
2020-02-10 23:34:52 +0300

    fix indentation

--- test_calculator.py
+++ test_calculator.py
@@ -14,4 +14,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-    assert 100 == calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
