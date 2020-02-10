85d45beef44e44bdea102d68e4957ff25e201579
hkitapci-swe <60547436+hkitapci-swe@users.noreply.github.com>
2020-02-10 23:38:21 +0300

    add test for division

--- test_calculator.py
+++ test_calculator.py
@@ -15,3 +15,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert  5 == calculator.divide(10, 2)
